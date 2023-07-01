495b1f58c14325278551ed144d032b6e6e7ab829
DarkLordikss <75681846+DarkLordikss@users.noreply.github.com>
2023-07-01 08:19:21 +0700

    ChainsawDialog
    -> Fix material need!

--- Assets/Materials/ChainsawDialogController.cs
+++ Assets/Materials/ChainsawDialogController.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LeshDialogControl : MonoBehaviour
+public class ChainsawDialogControl : MonoBehaviour
 {
     public PlayerControl player;
     public GameObject puzzle;
 
     private void Update()
     {
-        if (player.haveWorm)
+        if (player.brevnoCutted)
         {
             Destroy(gameObject);
             if (puzzle != null)
